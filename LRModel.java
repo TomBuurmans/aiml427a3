acb2e197365afeb81b5b9dc8c7b8df49d9d96261
pradhaprag <pradhaprag@ecs.vuw.ac.nz>
2021-06-07 21:29:21 +1200

    Added output file path in LR model

--- LRModel.java
+++ LRModel.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.classification.LogisticRegressionTrainingSummary;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.rdd.RDD;
@@ -19,14 +22,16 @@ public final class LRModel {
 	
 	static final long SPLIT_SEED = new Random().nextLong();
 	
-	private void init(String[] args) {
-		
+	private void init(String inputPath, String outputPath) {
+		long startTime = System.currentTimeMillis();
+
 		//configure spark
 		SparkConf conf = new SparkConf().setAppName("LogisticRegression").setMaster("local");
 	    SparkContext sc = new SparkContext(conf);
+	    JavaSparkContext  context = new JavaSparkContext(sc);
 
 	    // Load dataset
-		RDD<String> input = sc.textFile("kdd.data", 1);
+		RDD<String> input = sc.textFile(inputPath, 1);
 	    JavaRDD<String> data = input.toJavaRDD(); 
 	    
 	    //split dataset into training and test set with random seed
@@ -44,24 +49,54 @@ public final class LRModel {
 
 	    // Train Logistic Regression model
 	    org.apache.spark.mllib.classification.LogisticRegressionModel model = new LogisticRegressionWithLBFGS()
-	    	      .setNumClasses(10)
+	    	      .setNumClasses(2)
+	    	      .setIntercept(true)
 	    	      .run(trainLabeledPoints.rdd());  
 	    
 	    
-	    System.out.println("<<<<<<<<<<<<<<<<<<<<<<< TRAINING >>>>>>>>>>>>>>>>>>>>>>>>");
+	    System.out.println("<<<<<<<<<<<<<<<<<<<<<<< TESTING >>>>>>>>>>>>>>>>>>>>>>>>");
 	    
-	    //Testing the model
-	    JavaPairRDD<Object, Object> predictionAndLabels = testLabeledPoints.mapToPair(p ->
-        new Tuple2<>(model.predict(p.features()), p.label()));
-	    
-	    
-	    //calculates the accuracy
-	    MulticlassMetrics metrics = new MulticlassMetrics(predictionAndLabels.rdd());
-	    double accuracy = metrics.accuracy();
-	    System.out.println("Accuracy = " + accuracy);
-	    
-	    double testErr = 1.0 - accuracy;
-	    System.out.println("Test Error = "+ testErr );
+	  //Testing the model
+	  		JavaPairRDD<Object, Object> predictionAndLabelsTrain = trainLabeledPoints.mapToPair(p ->
+	          new Tuple2<>(model.predict(p.features()), p.label()));
+
+	  	    JavaPairRDD<Object, Object> predictionAndLabelsTest = testLabeledPoints.mapToPair(p ->
+	          new Tuple2<>(model.predict(p.features()), p.label()));
+	  	    
+	  	    //calculates the training accuracy
+	  		MulticlassMetrics metricsTrain = new MulticlassMetrics(predictionAndLabelsTrain.rdd());
+	  	    double trainAccuracy = metricsTrain.accuracy();
+	  	    System.out.println("Train Accuracy = " + trainAccuracy);
+	  	    
+	  	    double trainErr = 1.0 - trainAccuracy;
+	  	    System.out.println("Train Error = "+ trainErr );
+
+	  	    //calculates the test accuracy
+	  	    MulticlassMetrics metricsTest = new MulticlassMetrics(predictionAndLabelsTest.rdd());
+	  	    double testAccuracy = metricsTest.accuracy();
+	  	    System.out.println("Test Accuracy = " + testAccuracy);
+	  	    
+	  	    double testErr = 1.0 - testAccuracy;
+	  	    System.out.println("Test Error = "+ testErr );
+
+	  		System.out.print("Final w: " + model.weights());
+	  		System.out.println("LR Model\n" +model.toString());
+	  		
+	  		long timeTaken = (System.currentTimeMillis() - startTime) / 1000;
+	  		
+	  		 String outputString = "Train Accuracy: " + trainAccuracy + "\n";
+	 		outputString += "Train Error: " + trainErr + "\n";
+	 		outputString += "Test Accuracy: " + testAccuracy + "\n";
+	 		outputString += "Test Error: " + testErr + "\n";
+	 		outputString += "Duration: " + timeTaken + "ms\n";
+	 		outputString += "Final weights: "+ model.weights();
+	 		outputString += "Logistic Regression model: "+ model.toString();
+
+	         List<String> outputStrings = new ArrayList<String>();
+	 		outputStrings.add(outputString);
+	 		
+	 		JavaRDD<String> output = context.parallelize(outputStrings);
+	 		output.saveAsTextFile(outputPath);
 
 	    sc.stop();
 		
@@ -88,7 +123,7 @@ public final class LRModel {
 	public static void main(String[] args) {
 
 		LRModel runLR = new LRModel();
-		runLR.init(args);
+		runLR.init(args[0],args[1]);
 	}
 
 }
